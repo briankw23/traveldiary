ba27c82b845ed3d9fd58faf3be13380b2410ef34
Brian Wright <briankw23@gmail.com>
2018-03-21 19:24:38 -0500

    WIP created data object and gathered pics

---
+++ main.js
@@ -0,0 +1,10 @@
+let place = [
+    {location:"New York, NY", image:"<img src='newyork.jpg'>",description:"This place is awesome!"},
+    {location:"Chicago, IL",image:"<img src='chicago.jpg'>",description:"This place is awesome!"},
+    {location:"Portland, Oregon",image:"<img src='portland.jpg'>",description:"This place is awesome!"},
+    {location:"Banff, Alberta",image:"<img src='banff.jpg'>",description:"This place is awesome!"},
+    {location:"Seattle, Washington",image:"<img src='seattle.jpg'>",description:"This place is awesome!"},
+    {location:"Oakland, California",image:"<img src='oakland.jpg'>",description:"This place is awesome!"},
+    {location:"Milan, Italy",image:"<img src='milan.jpg'>",description:"This place is awesome!"},
+    {location:"Tokyo, Japan",image:"<img src='tokyo.jpg'>",description:"This place is awesome!"},
+];
