7572c8008084b3393c482469ca383e3d52e0c572
Brian Wright <briankw23@gmail.com>
2018-03-27 18:11:18 -0500

    WIP

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+const printToDom = (domString, divID) => {
+    document.getElementById(divID).innerHTML += domString;
+};
+
 let place = [
     {location:"New York, NY", image:"<img src='newyork.jpg'>",description:"This place is awesome!"},
     {location:"Chicago, IL",image:"<img src='chicago.jpg'>",description:"This place is awesome!"},
@@ -9,14 +13,37 @@ let place = [
     {location:"Tokyo, Japan",image:"<img src='tokyo.jpg'>",description:"This place is awesome!"},
 ];
 
-const placeStringBuilder = () => {
-    let placestring = "";
-place.forEach((place) => {
-    
-    place.location
-    place.image
-    place.description
-    <textarea name="" id="" cols="30" rows="10"></textarea> 
-    <button class="card-button">Brought Pie</button>
-})
+const placeStringBuilder = (array) => {
+    let domString= "";
+    for (let i = 0; i < array.length; i++) {
+        domString += `<div id="card">`;
+        domString += `<h2>${array[i].location}</h2>`;
+        domString += `<div class="img">${array[i].image}</div>`;
+        domString += `<p>${array[i].description}</p>`;
+        domString += `<textarea name="" id="" cols="30" rows="10"></textarea><br>` 
+        domString += `<button class="card-button">Post</button>`;
+        domString += `</div>`;
+        console.log(domString);
+}
+printToDom(domString,"main");
 };
+
+const addAllEventListeners = () => {
+    const allTheButtons = document.getElementsByClassName('card-button');
+
+    for (let i = 0; i < allTheButtons.length; i++) {
+        allTheButtons[i].addEventListener('click', postToDom);
+    }
+};
+
+const postToDom = (e) => {
+    const nameOfStudent = e.target.parentNode.children[0];
+    nameOfStudent.classList.add('green');
+};
+
+const startApplication = () =>{
+placeStringBuilder(place);
+addAllEventListeners();
+};
+
+startApplication();
